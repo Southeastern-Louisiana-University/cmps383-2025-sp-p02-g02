36ae49746727f52446e2752cb32f4c5e8998ecfa
VannaMoore <vannamoore@gmail.com>
2025-02-19 17:48:52 -0600

    Added User, Role, UserRole to DataContext class in DataContext.

--- Selu383.SP25.P02.Api/Data/DataContext.cs
+++ Selu383.SP25.P02.Api/Data/DataContext.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Selu383.SP25.P02.Api.Features.Roles;
 using Selu383.SP25.P02.Api.Features.Theaters;
+using Selu383.SP25.P02.Api.Features.UserRoles;
+using Selu383.SP25.P02.Api.Features.Users;
 
 namespace Selu383.SP25.P02.Api.Data
 {
@@ -10,5 +13,8 @@ namespace Selu383.SP25.P02.Api.Data
         }
 
         public DbSet<Theater> Theaters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
     }
 }
