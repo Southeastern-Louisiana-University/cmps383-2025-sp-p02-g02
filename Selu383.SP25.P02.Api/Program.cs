ca2e97d314ded53f0b08ddabf5aa6677dc3fceb8
VannaMoore <vannamoore@gmail.com>
2025-02-27 23:38:53 -0600

    Troubleshooting deployment issue. Changed connection string. All tests still pass.

--- Selu383.SP25.P02.Api/Program.cs
+++ Selu383.SP25.P02.Api/Program.cs
@@ -13,9 +13,7 @@ namespace Selu383.SP25.P02.Api
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")
-                ?? throw new InvalidOperationException("Connection string 'DataContext' not found.")));
+            builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
             builder.Services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<DataContext>();
             builder.Services.ConfigureApplicationCookie(options =>
